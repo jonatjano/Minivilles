5faa5ac1c3c4ec9df3d4b5426a52f1f9fa2f0c7a
Jonatjano <jonatjano@laposte.net>
2017-06-19 10:45:02 +0200

    added getter in Carte.java

--- Carte.java
+++ Carte.java
@@ -10,7 +10,6 @@ public class Carte
 	int 	cout;
 	int 	nbMax;
 
-
 	public Carte (String nom, String type, String description, int cout, int nbMax)
 	{
 		this.nom 			= nom;
@@ -20,8 +19,27 @@ public class Carte
 		this.nbMax			= nbMax;
 	}
 
-
 	/* GETTER */
+
+	public String getNom()
+	{
+		return this.nom;
+	}
+
+	public String getType()
+	{
+		return this.type;
+	}
+
+	public String getDescription()
+	{
+		return this.description;
+	}
+
+	public int getCout()
+	{
+		return this.cout;
+	}
 
 	public int getNbMax ()
 	{
@@ -33,7 +51,7 @@ public class Carte
 	{
 		String sRet = "";
 
-
+		sRet += cout + " " + type + " " + nom + " " + description;
 
 		return sRet;
 	}
