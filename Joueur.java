eaa0f73d7d454b3de76307e37a5279abdcab2eb2
Brice Mautalent <brice@Blowtekk-Debian>
2017-06-19 10:48:36 +0200

    La classe Joueur.java

---
+++ Joueur.java
@@ -0,0 +1,34 @@
+public class Joueur
+{
+	String prenom;
+
+	private ArrayList<Carte> etablissements;
+	private ArrayList<Carte> monuments;
+	private int monnaie;
+
+}
+
+public Joueur(String prenom)
+{
+	this.prenom = prenom;
+	this.monuments = new ArrayList<Carte>();
+	this.etablissements = new ArrayList<Carte>();
+	this.monnaie = 3;
+}
+
+public Joueur(String prenom, int monnaie)
+{
+	this.prenom = prenom;
+	this.monuments = new ArrayList<Carte>();
+	this.etablissements = new ArrayList<Carte>();
+	this.monnaie = monnaie;
+}
+
+public String getPrenom(){return this.prenom;}
+public ArrayList<Carte> getMonuments(){return this.monuments;}
+public ArrayList<Carte> getEtablissements(){return this.etablissements;}
+public int getMonnaie(){return this.monnaie;}
+
+public void setMonnaie(int valeur){this.monnaie+=valeur;}
+public void ajouterMonument(Carte monument){this.monuments.add(monument);}
+public void ajouterEtablissement(Carte etablissement){this.monuments.add(etablissement);}
